e1960e2b094f476d6c81849bb889ac3191a6ba65
Teddy Wing <git@teddywing.com>
2022-10-25 01:39:47 +0200

    Include CoreFoundation
    `Float32` and `OSStatus` are defined in CoreFoundation so we should
    include it.

--- SSVLSystemAlertVolume.h
+++ SSVLSystemAlertVolume.h
@@ -2,6 +2,7 @@
 #define SSVLSystemAlertVolume_h
 
 #include <AudioToolbox/AudioToolbox.h>
+#include <CoreFoundation/CoreFoundation.h>
 
 OSStatus SSVLGetSystemVolume(Float32 *volume);
 OSStatus SSVLSetSystemVolume(Float32 volume);
