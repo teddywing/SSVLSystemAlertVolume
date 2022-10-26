44265f9a98916c1fbd523ceb008e3b77fd4aaffb
Teddy Wing <git@teddywing.com>
2022-10-27 00:16:16 +0200

    Add a note to add documentation

--- SSVLSystemAlertVolume.h
+++ SSVLSystemAlertVolume.h
@@ -4,7 +4,11 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <CoreFoundation/CoreFoundation.h>
 
+// TODO: docs
+// Float between 0 an 1.
 OSStatus SSVLGetSystemVolume(Float32 *volume);
+
+// TODO: docs
 OSStatus SSVLSetSystemVolume(Float32 volume);
 
 #endif /* SSVLSystemAlertVolume_h */
