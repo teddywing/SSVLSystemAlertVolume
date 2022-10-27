a27b62c0db1498eea8a2bea6deabd256b275bccb
Teddy Wing <git@teddywing.com>
2022-10-27 02:44:57 +0200

    SSVLSystemAlertVolume.h: Add function documentation

--- SSVLSystemAlertVolume.h
+++ SSVLSystemAlertVolume.h
@@ -4,11 +4,18 @@
 #include <AudioToolbox/AudioToolbox.h>
 #include <CoreFoundation/CoreFoundation.h>
 
-// TODO: docs
-// Float between 0 an 1.
+// Get the system alert volume.
+//
+// Sets `volume` to the current system alert volume.
+//
+// Returns the result of `AudioServicesGetProperty`.
 OSStatus SSVLGetSystemVolume(Float32 *volume);
 
-// TODO: docs
+// Set the system alert volume.
+//
+// The input `volume` must be a float between 0 an 1.
+//
+// Returns the result of `AudioServicesSetProperty`.
 OSStatus SSVLSetSystemVolume(Float32 volume);
 
 #endif /* SSVLSystemAlertVolume_h */
